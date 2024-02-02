a80ca36b15b8ca343bf6c0dcaddc18cb52a9757f
Léa <konincks.lea@gmail.com>
2024-02-02 11:26:20 +0100

    multiprocessing works wooohoooooo !

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
 import re
 from src.scraper import Scraper
 from bs4 import BeautifulSoup
@@ -16,14 +18,28 @@ for country in countries:
 country = 'us' 
 print(f"Processing {country}") 
 s1.get_leaders(country) 
-leaders = s1.leaders_data 
-i = 0
-for leader in leaders:
-    i += 1
+first_paragraphs = []
+#i = 0
+for leader in s1.leaders_data:
+    #i += 1
     leader_name = leader ['first_name'] + " " + leader ['last_name']
     print(f"processing {leader_name}")
-    first_paragraph = s1.get_first_paragraph(leader ['wikipedia_url'])
-    leader_json[country][leader_name] = first_paragraph
-    if i == 5: break
+
+    #create a list of all the wikipedia urls
+    first_paragraphs.append(leader['wikipedia_url'])
+
+#run the threadpool on them
+print(f"Launching the map threading for the {country} country... Please be patient (grab a tea).")
+with ThreadPoolExecutor() as pool:
+    first_paragraphs = list(pool.map(s1.get_first_paragraph, first_paragraphs))
+
+i = 0
+print("adding it all to the dict")
+for leader in s1.leaders_data:
+    leader_name = leader ['first_name'] + " " + leader ['last_name']
+    leader_json[country][leader_name] = first_paragraphs[i]
+    i+=1
+
+print("Producing the json file.")
 with open("sample.json", "w") as outfile:
     outfile.write(json.dumps(leader_json, indent=4))
