429b34c5eb5fb734116aa4d0f4e79f667bdd2a5c
Léa <konincks.lea@gmail.com>
2024-02-02 11:40:37 +0100

    successfully implemented all data into the json file

--- main.py
+++ main.py
@@ -19,13 +19,9 @@ country = 'us'
 print(f"Processing {country}") 
 s1.get_leaders(country) 
 first_paragraphs = []
-#i = 0
 for leader in s1.leaders_data:
-    #i += 1
     leader_name = leader ['first_name'] + " " + leader ['last_name']
-    print(f"processing {leader_name}")
-
-    #create a list of all the wikipedia urls
+    #Create a list of all the wikipedia urls
     first_paragraphs.append(leader['wikipedia_url'])
 
 #run the threadpool on them
@@ -33,13 +29,12 @@ print(f"Launching the map threading for the {country} country... Please be patie
 with ThreadPoolExecutor() as pool:
     first_paragraphs = list(pool.map(s1.get_first_paragraph, first_paragraphs))
 
+#Append the first_paragraphs list to the dictionnary
 i = 0
-print("adding it all to the dict")
 for leader in s1.leaders_data:
-    leader_name = leader ['first_name'] + " " + leader ['last_name']
-    leader_json[country][leader_name] = first_paragraphs[i]
+    leader['first_wiki_paragraph'] = first_paragraphs[i]
     i+=1
 
 print("Producing the json file.")
 with open("sample.json", "w") as outfile:
-    outfile.write(json.dumps(leader_json, indent=4))
+    outfile.write(json.dumps(s1.leaders_data, indent=4))
